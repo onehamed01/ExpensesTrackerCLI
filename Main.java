c6a41ddb4e19a89cad5d927b03219c5cc0bf847b
onehamed01 <hami.mdv01@Gmail.com>
2026-06-26 10:17:07 +0100

    Add Tracking class to calculate and process each amount. process method sifts if amount less than zero. so it is declare as an expense or plus as income amount.

--- Main.java
+++ Main.java
@@ -1,5 +1,42 @@
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Hello Babe!");
+class Transaction{
+    private double amount;
+
+    Transaction(double amount){
+        this.amount = amount;
     }
+    double getterAmount(){
+        return this.amount;
+    }
+}
+class Tracking{
+    private double save;
+    private double expenses;
+    Transaction tObj;
+
+    Tracking(Transaction tObj) {
+        this.save = 0;
+        this.expenses = 0;
+        this.tObj = tObj;
+    }
+    void showing(){
+        System.out.println("Current Save: £"+ this.save);
+        System.out.println("Current Expenses: £"+ this.expenses);
+
+    }
+
+    void process(){
+        double currentAmount = tObj.getterAmount();
+
+        if (currentAmount > 0){
+        }
+        else if (tObj.getterAmount() < 0) {
+            currentAmount += this.expenses; 
+            showing();
+        } 
+        else {
+            System.out.println("Invalid amount!");
+            showing();
+        }
+    }
+
 }
