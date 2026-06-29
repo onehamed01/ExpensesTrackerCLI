240c25e69efb2a88ad528a2eaa04fe7222426ad0
onehamed01 <hami.mdv01@Gmail.com>
2026-06-29 12:17:56 +0100

    Add processor method to check and implement each object of transaction. There is a bug for System out message to show if epenses calcuation euqail to zero.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 // The logic is one-to-many. Transaction's objects can implement by one Tracker to process the logic 
 
 class Transaction{
@@ -24,6 +25,47 @@ class Tracker{
     }
  // Process each Transaction object is it income or outcome. Each Tracker object has multiple Transaction. So it should save the last calculation for next Transaction's object
     void processor(Transaction transactionObject){
+        double amount = transactionObject.getAmount();
 
+        if (amount > 0){
+            Scanner income = new Scanner(System.in);
+            System.out.println("How much you would like to save with £"+ amount + ": ");
+            double incomeInput = income.nextDouble();
+
+            // Add new amount to the last save value 
+            this.save += incomeInput;
+
+            // deduce save from total amount to fetch expenses
+            this.expenses += amount - incomeInput;
+        }
+        // Else the amount be expenses (negative amount)
+        else{
+            // check if the amount of expenses is bigger than the last amount of expenses 
+            if (this.expenses < amount) {
+                System.out.println("You don't have enought amount to pay!");
+            }
+            else if (this.expenses - amount == 0) {
+                this.expenses = 0;
+                System.out.println("you current expenses amount is 0. Please charge it!");
+            }
+            else{
+                this.expenses += amount;
+            }
+        }
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+     Tracker tracker = new Tracker();
+     
+     // Income instance
+     Transaction thisWeekIncome = new Transaction(280.00);
+     Transaction thisWeekTip = new Transaction(35.00);
+     Transaction liddleShopping = new Transaction(-80.00);
+     tracker.processor(thisWeekIncome);
+     tracker.processor(thisWeekTip);
+     tracker.processor(liddleShopping);
+     tracker.summery();
     }
 }
